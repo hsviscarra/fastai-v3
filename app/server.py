e50b397765dc7fcbe900e2640a29d041d96107be
Hernan Viscarra A <hsviscarra@gmail.com>
2019-03-01 19:49:07 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/m1mpzvwbieqst2f/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['rich', 'poor']
 path = Path(__file__).parent
 
 app = Starlette()
